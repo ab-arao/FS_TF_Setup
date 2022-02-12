e367c1685bd18b63c95c8ccd29dc2e1989ef99aa
Doug Smith <superdug@gmail.com>
2022-02-12 12:57:32 -0600

    gitignore

--- variable.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_access_key" {
-default = "AKIA56ZIAIO4UQB5422H"
-}
-variable "aws_secret_key" {
-default = "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"
- }
-variable "region" {
-    default = "us-west-1"
-}
