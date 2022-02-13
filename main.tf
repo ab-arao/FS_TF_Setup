f283a4900ae15351bb1b5c8c462a71ccccbb4aab
Doug Smith <superdug@gmail.com>
2022-02-13 15:47:50 -0600

    Cloud Front configured for domain amiblocked.io

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.74.2"
+    }
+  }
+}
+
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
-    region = "${var.region}"
-}
-
-// Create a variable for our domain name because we'll be using it a lot.
-variable "www_domain_name" {
-  default = "www.amiblocked.io"
-}
-
-// We'll also need the root domain (also known as zone apex or naked domain).
-variable "root_domain_name" {
-  default = "amiblocked.io"
+    region = var.region
 }
 
 module "ses-email-forwarding" {
@@ -23,6 +22,6 @@ module "ses-email-forwarding" {
     s3_bucket_prefix = "emails"
     mail_targets     = ["test@amiblocked.io", "administrator@amiblocked.io", "hostmaster@amiblocked.io", "postmaster@amiblocked.io", "webmaster@amiblocked.io", "admin@amiblocked.io"]
     mail_sender      = "postmaster@amiblocked.io"
-    mail_recipient   = "fluentsteam@dugnet.com"
+    mail_recipient   = "fs@dugnet.com"
     aws_region       = "${var.region}"
 }
--- s3website.tf
+++ s3website.tf
@@ -1,42 +1,168 @@
-resource "aws_s3_bucket" "www" {
+resource "aws_s3_bucket" "b" {
   bucket = "${var.root_domain_name}"
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.www.bucket
   acl    = "public-read"
+  policy = <<EOF
+{
+  "Version":"2012-10-17",
+  "Statement":[{
+        "Sid":"PublicReadForGetBucketObjects",
+        "Effect":"Allow",
+          "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::${var.root_domain_name}/*"]
+    }
+  ]
+}
+EOF
+
+  force_destroy = true
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 }
 
-resource "aws_s3_bucket_website_configuration" "example" {
-  bucket = aws_s3_bucket.www.bucket
+resource "aws_s3_bucket_object" "object1" {
+  for_each = fileset("website/", "*")
+  bucket = aws_s3_bucket.b.id
+  key = each.value
+  source = "website/${each.value}"
+  etag = filemd5("website/${each.value}")
+  content_type = "text/html"
+}
 
-  index_document {
-    suffix = "index.html"
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "${var.root_domain_name}"
+  validation_method = "DNS"
+
+  tags = {
+    Environment = "test"
   }
 
-  error_document {
-    key = "error.html"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-resource "aws_s3_object" "index" {
- bucket = aws_s3_bucket.www.bucket
+data "aws_route53_zone" "zone" {
+  name         = "${var.root_domain_name}"
+  private_zone = false
+}
 
-  key = "index.html"
+// Route 53 Record
+ resource "aws_route53_record" "www" {
+   for_each = {
+     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+       name   = dvo.resource_record_name
+       record = dvo.resource_record_value
+       type   = dvo.resource_record_type
+     }
+   }
 
-  acl = "public-read"
-  source = "${path.module}/website/index.html" 
+   allow_overwrite = true
+   name            = each.value.name
+   records         = [each.value.record]
+   ttl             = 60
+   type            = each.value.type
+   zone_id         = data.aws_route53_zone.zone.zone_id
+ }
 
-  content_type = "text/html"
-} 
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = "${aws_acm_certificate.cert.arn}"
+  validation_record_fqdns = [for record in aws_route53_record.www : record.fqdn]
+}
 
-resource "aws_s3_object" "error" {
-  bucket = aws_s3_bucket.www.bucket
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.b.bucket_regional_domain_name
+    origin_id   = "${var.root_domain_name}"
+  }
 
-  key = "error.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
 
-  acl = "public-read"
-  source = "${path.module}/website/error.html"
+   custom_error_response {
+      error_caching_min_ttl = 3000
+      error_code = 404
+      response_code = 200
+      response_page_path = "/error.html"
+  }
 
-  content_type = "text/html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "${var.root_domain_name}.s3.amazonaws.com"
+    prefix          = "cloudfront_logs"
+  }
+
+  aliases = ["${var.root_domain_name}"]
+
+  default_cache_behavior {
+     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+     cached_methods   = ["GET", "HEAD"]
+     target_origin_id = "${var.root_domain_name}"
+  
+
+    forwarded_values {
+        query_string = false
+        cookies {
+          forward = "none"
+        }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "allow-all"
+  }
+
+  ordered_cache_behavior {
+     path_pattern     = "/*"
+     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+     cached_methods   = ["GET", "HEAD", "OPTIONS"]
+     target_origin_id = "${var.root_domain_name}"
+
+     forwarded_values {
+       query_string = false
+       headers      = ["Origin"]
+
+       cookies {
+         forward = "none"
+       }
+     }
+
+     min_ttl                = 0
+     default_ttl            = 86400
+     max_ttl                = 31536000
+     compress               = true
+     viewer_protocol_policy = "redirect-to-https"
+   }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.cert.arn
+    ssl_support_method = "sni-only"
+  }
+}
+
+resource "aws_route53_record" "record_a" {
+   zone_id = data.aws_route53_zone.zone.id
+   name    = "${var.root_domain_name}"
+   type    = "A"
+
+   alias {
+     name                   = replace(aws_cloudfront_distribution.s3_distribution.domain_name, "/[.]$/", "")
+     zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+     evaluate_target_health = true
+   }
 }
