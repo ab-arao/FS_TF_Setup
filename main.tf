0e73cc9525882127ee58c0fcb17d30cc4ce06597
Doug Smith <superdug@gmail.com>
2022-02-16 06:10:01 -0600

    specifiy pgsql

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "aurora-serverless" {
     cidr_block       = "10.0.0.0/16"
     database_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   }
-  name = "amiblocked-api-db"
+  name = "amiblocked-api-db-pgsql"
 }
 
 data "aws_availability_zones" "current" {
